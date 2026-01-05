1450f20fa0bfc7bd91b0772d013fbb37866fb97f
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-01-05 22:45:10 +0100

    chore(secrets): switch all generated secrets to hex format

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -14,15 +14,28 @@ sudo rpm-ostree install "${packages[@]}" --apply-live >/dev/null || true
 # --------------------------------------------------
 
 # PostgreSQL
-openssl rand -base64 36 | tr -d '\n' | podman secret create --ignore POSTGRES_PASSWORD -
-openssl rand -base64 36 | tr -d '\n' | podman secret create --ignore AUTHENTIK_POSTGRESQL__PASSWORD -
-openssl rand -base64 36 | tr -d '\n' | podman secret create --ignore LINKWARDEN_POSTGRESQL__PASSWORD -
+openssl rand -hex 16 | podman secret create --ignore POSTGRES_PASSWORD -
 
 # Authentik
-openssl rand -base64 60 | tr -d '\n' | podman secret create --ignore AUTHENTIK_SECRET_KEY -
+openssl rand -hex 32 | podman secret create --ignore AUTHENTIK_SECRET_KEY -
+openssl rand -hex 16 | podman secret create --ignore AUTHENTIK_POSTGRESQL__PASSWORD -
 
 # Caddy (prompted)
 read -r -s -p "Enter Caddy Clouflare DNS API token: " TOKEN
 echo
 printf '%s' "$TOKEN" | podman secret create --ignore CLOUDFLARE_API_TOKEN -
 unset TOKEN
+
+# Linkwarden
+openssl rand -hex 32 | podman secret create --ignore NEXTAUTH_SECRET -
+openssl rand -hex 32 | podman secret create --ignore MEILI_MASTER_KEY -
+
+DB_PASSWORD="$(openssl rand -hex 16)"
+
+printf '%s' "$DB_PASSWORD" |
+  podman secret create --ignore LINKWARDEN_POSTGRESQL__PASSWORD -
+
+printf 'postgresql://linkwarden:%s@postgres:5432/linkwarden' "$DB_PASSWORD" |
+  podman secret create --ignore LINKWARDEN_DATABASE_URL -
+
+unset DB_PASSWORD
